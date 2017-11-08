e5f918fd0a32a62cd992f1b873685dda94aa9677
Jody Duke <contact@jodyduke.co.uk>
2017-11-08 22:54:45 +0000

    added functionality - still loads to do

--- js/calculator.js
+++ js/calculator.js
@@ -1,27 +1,36 @@
+function looper(a, b){
+  b.innerHTML = "";
+  a.forEach(function(e){
+      b.innerHTML += e;
+      b.innerHTML += " ";
+  });
+}
+
 function calculator(){
-  let memory = [];
+  let memory = [],
+  tracker = document.getElementById('tracker'),
+  inputEl = document.getElementById('input'),
+  input = "";
 
-  let buttons = document.querySelectorAll('.button')
-
-  for(i = 0; i < buttons.length ; i++){
-    buttons[i].addEventListener('click', function(){
-      let input = document.getElementById('input');
-      switch (this.value){
-        case 'clear':
-          memory = [];
-          break;
-        case '=':
-          memory.push(input.value)
-          console.log(eval(memory.join('')))
-          input.value = '';
-          break;
-        default:
-          memory.push(input.value)
-          memory.push(this.value)
-          input.value = '';
+  document.getElementById('container').addEventListener('click', function(event){
+    //event.target.value;
+      if(event.target.value.match(/[.0123456789]/g)) {
+        input += event.target.value;
+        inputEl.innerHTML = input;
       }
-    })
-  }
+      else if(event.target.value.match(/[*/+-]/g)) {
+        memory.push(inputEl.innerHTML, event.target.value);
+        inputEl.innerHTML = "";
+        input = "";
+        looper(memory, tracker);    
+      }
+      else if(event.target.value === "="){
+        memory.push(inputEl.innerHTML);
+        inputEl.innerHTML = eval(memory.join(''));
+        looper(memory, tracker); 
+      }
+
+  });
 }
 
 calculator()
